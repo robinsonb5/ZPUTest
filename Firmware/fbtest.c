40dd43744442c3cd89cf89e50928e8645c3b1316
Alastair M. Robinson <robinsonb5@talk21.com>
2013-04-25 22:59:58 +0100

    Added new test, ZPU core runs at over 200Mhz

---
+++ Firmware/fbtest.c
@@ -0,0 +1,54 @@
+#include "minisoc_hardware.h"
+
+#define COUNTER *(volatile unsigned int *)(0xFFFFFF80)
+#define SWITCHES *(volatile unsigned int *)(0xFFFFFF84)
+
+#define FRAMEBUFFER 0x10000
+#define FB_WIDTH 640
+#define FB_HEIGHT 480
+
+void putserial(char *msg)
+{
+	while(*msg)
+	{
+		while(!(HW_PER(PER_UART)&(1<<PER_UART_TXREADY)))
+			;
+		HW_PER(PER_UART)=*msg++;
+	}
+}
+
+
+void ClearFramebuffer()
+{
+	int *fb=(int *)FRAMEBUFFER;
+	int c=FB_WIDTH*FB_HEIGHT/4;
+	while(--c)
+		*fb++=0;
+}
+
+
+void FillFramebuffer(int s)
+{
+	int *fb=(int *)FRAMEBUFFER;
+	int c=FB_WIDTH*FB_HEIGHT/4;
+	while(--c)
+		*fb++=s++;
+}
+
+
+int main(int argc,char *argv)
+{
+	int c=0;
+	long p=0;
+	HW_PER(PER_UART_CLKDIV)=1000000/1152;
+	putserial("Hello world!\n");
+	HW_VGA(FRAMEBUFFERPTR)=FRAMEBUFFER;
+	ClearFramebuffer();
+	while(1)
+	{
+		HW_PER(PER_HEX)=c;
+		FillFramebuffer(c);
+	}
+	return(0);
+}
+
