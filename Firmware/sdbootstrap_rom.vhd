ec316625a715712dfcdf4e41273b8dc9202c1308
Alastair M. Robinson <robinsonb5@talk21.com>
2013-05-27 10:08:59 +0100

    fixed filenames

--- Firmware/sdbootstrap_rom.vhd
+++ Firmware/sdbootstrap_rom.vhd
@@ -981,3 +981,28 @@ shared variable ram : ram_type :=
    917 => x"46415433",
    918 => x"32202020",
    919 => x"00000000",
+	others => x"00000000"
+);
+
+begin
+
+mem_busy<=mem_readEnable; -- we're done on the cycle after we serve the read request
+
+process (clk, areset)
+begin
+		if areset = '1' then
+		elsif (clk'event and clk = '1') then
+			if (mem_writeEnable = '1') then
+				ram(to_integer(unsigned(mem_addr(maxAddrBit downto minAddrBit)))) := mem_write;
+			end if;
+		if (mem_readEnable = '1') then
+			mem_read <= ram(to_integer(unsigned(mem_addr(maxAddrBit downto minAddrBit))));
+		end if;
+	end if;
+end process;
+
+
+
+
+end arch;
+
