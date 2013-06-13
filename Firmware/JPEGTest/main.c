e818cfe03660ef8bde50ad6ac2a91c1aa6b72e57
Alastair M. Robinson <robinsonb5@talk21.com>
2013-06-13 01:10:47 +0100

    First JPEG successfully loaded

--- Firmware/JPEGTest/main.c
+++ Firmware/JPEGTest/main.c
@@ -4,6 +4,8 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
+#include "jpeglib.h"
+
 #include "fat.h"
 #include "small_printf.h"
 
@@ -13,35 +15,75 @@ char string[]="Hello world!\n";
 
 static struct stat statbuf;
 
+
+struct JPEGContext
+{
+	struct jpeg_decompress_struct cinfo;
+	struct jpeg_error_mgr jerr;
+	int fd;
+	JSAMPROW rowptr[1];
+};
+
+void jpeg_unixio_src (j_decompress_ptr cinfo, int infile);
+
 int main(int argc, char **argv)
 {
 	file=malloc(sizeof(fileTYPE));
+	struct JPEGContext *jc=malloc(sizeof(struct JPEGContext));
 
-#if 0
-	FILE *f=fopen("SPLASH  RAW","rb");
-	printf("fopen() returned %d\n",f);
-	if(f)
-	{
-		char *imagebuf;
-		if((imagebuf=malloc(640*480*2)))
-		{
-			printf("Got image buffer\n");
-			HW_VGA(FRAMEBUFFERPTR)=imagebuf;
-			fread(imagebuf,1,640*480*2,f);
-		}
-	}
-#endif
+	jc->cinfo.err = jpeg_std_error(&jc->jerr);
+	jpeg_create_decompress(&jc->cinfo);
+	printf("created decompress\n");
+	jc->fd=open("80SDIVE JPG",0,O_RDONLY);
+	printf("open() returned %d\n",jc->fd);
 
-	int fd=open("80SDIVE JPG",0,O_RDONLY);
-	printf("open() returned %d\n",fd);
-	if((fd>0)&&!fstat(fd,&statbuf))
+	if((jc->fd>0)&&!fstat(jc->fd,&statbuf))
 	{
 		char *imagebuf;
 		printf("File size: %d\n",statbuf.st_size);
-		if((imagebuf=malloc(statbuf.st_size)))
+		jpeg_unixio_src(&jc->cinfo,jc->fd);
+		printf("Added unixio source\n");
+		if((imagebuf=malloc(640*480*2)))
 		{
+			int row;
+			JOCTET *rowbuffer;
+			int *imagep=(int *)imagebuf;
 			HW_VGA(FRAMEBUFFERPTR)=imagebuf;
-			read(fd,imagebuf,statbuf.st_size);
+
+			jpeg_read_header(&jc->cinfo, TRUE);
+			printf("Read JPEG header - dimensions %d x %d\n",jc->cinfo.output_width,jc->cinfo.output_height);
+			jpeg_start_decompress(&jc->cinfo);
+			printf("Started decompress\n");
+			printf("dimensions %d x %d\n",jc->cinfo.output_width,jc->cinfo.output_height);
+			printf("components: %d\n",jc->cinfo.output_components);
+			rowbuffer=malloc(jc->cinfo.output_width*jc->cinfo.output_components);	// Read data is 24 bits.
+
+			for(row=0;row<jc->cinfo.output_height;++row)
+			{
+				JOCTET *t=rowbuffer;
+				jc->rowptr[0]=rowbuffer;
+				int x,w;
+				int scanlines=jpeg_read_scanlines(&jc->cinfo,&jc->rowptr,1);
+				printf("Scanlines read: %d\n",scanlines);
+				for(x=0;x<jc->cinfo.output_width;x+=2)
+				{
+					int r=*t++;
+					int g=*t++;
+					int b=*t++;
+					w=(r&0xf8)<<24;
+					w|=(g&0xfc)<<19;
+					w|=(b&0xf8)<<13;
+					r=*t++;
+					g=*t++;
+					b=*t++;
+					w|=(r&0xf8)<<8;
+					w|=(g&0xfc)<<3;
+					w|=(b&0xf8)>>3;
+					*imagep++=w;
+				}
+			}
+
+//			read(fd,imagebuf,statbuf.st_size);
 		}
 	}
 
